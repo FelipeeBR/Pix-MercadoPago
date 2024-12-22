42e77f2020611ca7cdbc162062ccde3580edd8cb
Felipe Mendes Neves <felipemen74@gmail.com>
2024-12-22 18:15:17 -0300

    autenticação navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,38 +10,46 @@ import { useSession } from "next-auth/react";
 const Navbar = () => {
     const {data:session} = useSession();
     return (
-        <div className="flex justify-between items-center w-9/12 h-20 px-4 text-black bg-grey-700 rounded nav border border-slate-200 shadow-md">
-            <div>
-                <h1 className="text-2xl font-signature ml-2">
-                    <Link href={"/home"}>
-                        <SiHomebridge size={50}/>
-                    </Link>
-                </h1>
-            </div>
-            <div>
-                {session ? (
-                    <h3 className="md:text-2xl font-bold flex">
-                        <FaUserCircle size={30}/>
-                        {(session as any).user?.name}
-                    </h3>
-                ): (
-                    <h3 className="md:text-2xl font-bold">
-                        Entrar
-                    </h3>
-                )}
-            </div>
-            <div>
-                <Link href={"/compras"}>
-                <h3 className="md:text-2xl font-bold flex">
-                    <FaCartShopping size={30}/>
-                    Minhas Compras
-                </h3>
-                </Link>
-            </div>
-            <div>
-                <ButtonLogout/>
-            </div>
-      </div>
+        <div>
+            {session ? (
+                <div className="flex justify-between items-center w-9/12 h-20 px-4 text-black bg-grey-700 rounded nav border border-slate-200 shadow-md">
+                    <div>
+                        <h1 className="text-2xl font-signature ml-2">
+                            <Link href={"/home"}>
+                                <SiHomebridge size={50}/>
+                            </Link>
+                        </h1>
+                    </div>
+                    <div>
+                        {session ? (
+                            <h3 className="md:text-2xl font-bold flex">
+                                <FaUserCircle size={30}/>
+                                {(session as any).user?.name}
+                            </h3>
+                        ): (
+                            <h3 className="md:text-2xl font-bold">
+                                Entrar
+                            </h3>
+                        )}
+                    </div>
+                    <div>
+                        <Link href={"/compras"}>
+                        <h3 className="md:text-2xl font-bold flex">
+                            <FaCartShopping size={30}/>
+                            Minhas Compras
+                        </h3>
+                        </Link>
+                    </div>
+                    <div>
+                        <ButtonLogout/>
+                    </div>
+                </div>
+            ): (
+                <div>
+                    <h1 className="text-3xl font-bold">Entrar</h1>
+                </div>
+            )}
+        </div>
     )
 }
 
